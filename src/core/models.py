470bbf67c53e29567d765d830665409815f9b210
saniyaagrawal <saniya.agrawal30@gmail.com>
2020-05-27 18:20:36 +0530

    commit

---
+++ src/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Author(models.Model):
+    name= models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name= models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Journal(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    publish_date = models.DateTimeField()
+    views = models.IntegerField(default=0)
+    reviewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
